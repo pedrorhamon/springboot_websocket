7d04b0874b7d3a59b0712b4578b5d40f81b6e3b7
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-01-03 19:56:13 -0300

    Configurando endpoint WebSocket

---
+++ src/main/java/com/starking/ws/handler/WebSocketHandler.java
@@ -0,0 +1,29 @@
+package com.starking.ws.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		System.out.println("[afterConnectionEstablished] session id " + session.getId());
+//		super.afterConnectionEstablished(session);
+	}
+
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		System.out.println("[handleTextMessage] message " + message.getPayload());
+//		super.handleTextMessage(session, message);
+	}
+
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		System.out.println("[afterConnectionClosed] session id " + session.getId());
+//		super.afterConnectionClosed(session, status);
+	}
+}
