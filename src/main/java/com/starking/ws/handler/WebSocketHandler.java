aa7d8154272fbc530c72e8719f7e918d1aacbe0a
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-01-05 20:34:41 -0300

    Spring Boot com Redis como cache

--- src/main/java/com/starking/ws/handler/WebSocketHandler.java
+++ src/main/java/com/starking/ws/handler/WebSocketHandler.java
@@ -1,10 +1,5 @@
 package com.starking.ws.handler;
 
-import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.UUID;
-
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -13,35 +8,19 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        System.out.println("[afterConnectionEstablished] session id " + session.getId());
+    }
 
-	@Override
-	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		System.out.println("[afterConnectionEstablished] session id " + session.getId());
-//		super.afterConnectionEstablished(session);
-		
-		new Timer().scheduleAtFixedRate(new TimerTask() {
-			
-			@Override
-			public void run() {
-				if(!session.isOpen()) return;; 
-				try {
-					session.sendMessage(new TextMessage("Olá" + UUID.randomUUID()));					
-				}catch(IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}, 20000L, 20000L);
-	}
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) {
+        System.out.println("[handleTextMessage] message " + message.getPayload());
+    }
 
-	@Override
-	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		System.out.println("[handleTextMessage] mensagem " + message.getPayload());
-//		super.handleTextMessage(session, message);
-	}
-
-	@Override
-	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		System.out.println("[afterConnectionClosed] session id " + session.getId());
-//		super.afterConnectionClosed(session, status);
-	}
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        System.out.println("[afterConnectionClosed] session id " + session.getId());
+    }
 }
+
