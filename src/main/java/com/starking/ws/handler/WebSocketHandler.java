49ad9ce02f217dfea89d89bdb7e82eae6404d53c
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-01-11 18:13:59 -0300

    Protegendo conexões WebSocket com tickets

--- src/main/java/com/starking/ws/handler/WebSocketHandler.java
+++ src/main/java/com/starking/ws/handler/WebSocketHandler.java
@@ -1,26 +1,72 @@
 package com.starking.ws.handler;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.starking.ws.services.TicketService;
 
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
+	
+	private final static Logger LOGGER = Logger.getLogger(WebSocketHandler.class.getName());
+	
+	private final TicketService ticketService;
+	
+	private final Map<String, WebSocketSession> sessions;
+	
+	public WebSocketHandler(TicketService ticketService) {
+		this.ticketService = ticketService;
+		sessions = new ConcurrentHashMap<>();
+	}
+	
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
-        System.out.println("[afterConnectionEstablished] session id " + session.getId());
+        LOGGER.info("[afterConnectionEstablished] session id " + session.getId());
+        Optional<String> ticket = ticketOf(session);
+        if(ticket.isEmpty() || ticket.get().isBlank()) {
+        	LOGGER.warning("session: " + session.getId() + "without ticket");
+        	session.close();
+        	return
+        }
+    }
+    
+    private void close(WebSocketSession session) {
+    	try {
+			session.close(CloseStatus.POLICY_VIOLATION);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    private Optional<String> ticketOf(WebSocketSession session) {
+    	return Optional.ofNullable(session.getUri())
+    			.map(UriComponentsBuilder::fromUri)
+    			.map(UriComponentsBuilder::build)
+    			.map(UriComponents::getQueryParams)
+    			.map(it -> it.get("ticket"))
+    			.flatMap(it -> it.stream().findFirst())
+    			.map(String::trim);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) {
-        System.out.println("[handleTextMessage] message " + message.getPayload());
+    	 LOGGER.info("[handleTextMessage] message " + message.getPayload());
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-        System.out.println("[afterConnectionClosed] session id " + session.getId());
+    	 LOGGER.info("[afterConnectionClosed] session id " + session.getId());
     }
 }
 
